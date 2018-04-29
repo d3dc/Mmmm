f2b2dd6d14032b2c0694c79ccfab7dc7d21c80ef
Sam Richard <sam.richard@gmail.com>
2018-04-29 16:50:27 -0600

    Provide a node prompt alternative if we don't have window.prompt

--- runtime-mmmm/lib/runtime.js
+++ runtime-mmmm/lib/runtime.js
@@ -1,6 +1,10 @@
 const m = require('./m')
 const counter = require('./counter')
 
+const prompt = global.window
+  ? window.prompt
+  : require('readline-sync').prompt
+
 // TODO: Does this like have to be boxed?
 // Everything would use MValue and need to be unboxed...
 Number.prototype.m = function () { return this % 10 }
@@ -15,27 +19,24 @@ function makeInstructions (ops) {
 
 module.exports = function runtime (args) {
   const inputs = args || []
-  const out = ''
-  const stack = []
 
   const memory = {
-    o: out,
-    d: stack,
-    i: inputs.slice()
+    d: [],
+    o: ''
   }
 
   const instructions = makeInstructions({
     0: (a) => a + 1,
     1: () => counter(),
-    2: (a) => { stack.push(a); return stack.length },
-    3: () => stack.pop(),
+    2: (a) => { memory.d.push(a); return memory.d.length },
+    3: () => memory.d.pop(),
     4: () => inputs.pop() || prompt(),
-    5: (a) => out += a,
-    6: (a) => out += String.fromCharCode(a),
+    5: (a) => memory.o += a,
+    6: (a) => memory.o += String.fromCharCode(a),
     7: (a, b) => a ? b.m() : 0,
     8: (a) => (a < 1) ? 0 : a - 1,
     9: (a) => Math.random()
   })
 
-  return Object.assign({ m }, memory, instructions)
+  return Object.assign(memory, { m }, instructions)
 }
